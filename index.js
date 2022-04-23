62cd04b82ba3a339b00c5c76f87b80ce0f42a61c
Sam Wallace <sam@sammymayhem.com>
2022-04-22 21:43:38 -0700

    got Employee.js to pass all tests

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const employee = require('./lib/Employee');
+const engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+const manager = reauire('./lib/Manager');
+
+const template = require('./src/template');
+
