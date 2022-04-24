813ddb6cec8965303ccfbea7cd856c594b34979f
Sam Wallace <sam@sammymayhem.com>
2022-04-24 14:16:12 -0700

    build structure for selecting next employee

--- index.js
+++ index.js
@@ -31,6 +31,42 @@ function empManager(manager) {
             name: 'managersOfficeNumber',
             message: "What is the Manager's office number?"
         },
-    ]);
+    ])
+    .then((answers) => {
+        const manager = new Manager (
+            answers.managerName,
+            answers.managerId,
+            answers.managerEmail,
+            answers.managerOfficeNumber,
+        );
+        dreamTeam.push(manager);
+        nextEmp();
+    });
 }
-empManager();
+
+function nextEmp() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'nextEmployee',
+            message: "would you like to add another team member?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Assemble my team!",
+            ]
+        }
+    ])
+    .then(empChoice => {
+        switch(empChoice.nextEmployee) {
+            case "Engineer":
+                getEngineer();
+                break;
+            case "Intern":
+                getIntern();
+                break;
+            default:
+                teamHTML();
+        }
+    });
+}
